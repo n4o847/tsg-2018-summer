1bea2693a6de18de87ff985801a1282c87e30911
n4o847 <yuu.miu.0925@gmail.com>
2018-09-23 04:41:33 +0900

    整備

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # tsg-2018-summer
 
 https://gist.github.com/hakatashi/8e3dfb0e8fd6c00b165a4ca65ffed95c
+
+## copos
+
+[README](./copos/README.md)
+
+## golfish
+
+[README](./golfish/README.md)
--- generate.rb
+++ generate.rb
@@ -2,10 +2,10 @@ def coposify(code)
   code.chars.map{|c| ?# * c.ord } * $/
 end
 
-IO.binwrite "./hello.copos-rb", coposify('puts"Hello, world!"')
+IO.binwrite "./copos/hello.copos-rb", coposify('puts"Hello, world!"')
 
-IO.binwrite "./cat.copos-rb", coposify('$><<STDIN.read')
+IO.binwrite "./copos/cat.copos-rb", coposify('$><<STDIN.read')
 
-IO.binwrite "./hello.golfish", "\x30Hello, world!\n\xf4\xff"
+IO.binwrite "./golfish/hello.golfish", "\x30Hello, world!\n\xf4\xff"
 
-IO.binwrite "./cat.golfish", "\x03\x05\xff"
+IO.binwrite "./golfish/cat.golfish", "\x03\x05\xff"
